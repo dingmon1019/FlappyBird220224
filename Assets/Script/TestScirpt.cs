480c92bf5ac18b631b402a0aa78d4fa80b11317d
JunSoo <jsjsjsjsjs1019@naver.com>
2022-02-27 13:47:19 +0900

    Test변경사항입니다.
    별거없으니 인게임에서 삭제해도 별 문제없습니다

--- Assets/Script/TestScirpt.cs
+++ Assets/Script/TestScirpt.cs
@@ -2,15 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MonoBehaviour
+public class TestScirpt : MonoBehaviour
 {
-    public GameObject TestSquare;
     // Start is called before the first frame update
     void Start()
     {
-       GameObject NewGameObject= Instantiate(TestSquare);
-       NewGameObject.tag="Player";
-       NewGameObject.layer=LayerMask.NameToLayer("Water");
+        
     }
 
     // Update is called once per frame
